db657fa0dc86e2a0024f3fab3c3ccfdc8c9f738d
sagar jaid <sagar@sagarjaid.local>
2023-03-16 11:13:27 +0530

    api route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Configuration, OpenAIApi } from "openai";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const handler = async (req, res) => {
+  // const configuration = new Configuration({
+  //   apiKey: process.env.OPENAI_API_KEY,
+  // });
+
+  const configuration = new Configuration({
+    apiKey: "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",
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "how are you?" }],
+  });
+  console.log(completion.data.choices[0].message);
+
+  res.status(200).json({ name: completion.data.choices[0].message });
+};
+
+export default handler;
