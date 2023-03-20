6ef2ec998baa747bd2534c923fd26888c1cdeeda
Jaid Sagar Balu <jaid.s@photon.com>
2023-03-20 14:59:53 +0530

    VA added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <GoogleAnalytics trackPageViews />
+      <GoogleAnalytics  />
+      <Analytics />
     </>
   );
 }
