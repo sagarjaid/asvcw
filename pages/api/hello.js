96019b81ff681e9ba630605a7bc7c58d662e9587
Jaid Sagar Balu <jaid.s@photon.com>
2023-03-20 16:52:14 +0530

    new NEXT API KEY 2

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,21 +3,22 @@ import { Configuration, OpenAIApi } from 'openai';
 const configuration = new Configuration({
   apiKey: process.env.NEXT_PUBLIC_API_KEY
 });
+
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(
   req,
   res
 ) {
-  const completion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: req.body.text,
-    temperature: 0.7,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    max_tokens: 256,
-  });
-  console.log(completion.data);
-  res.status(200).json({ result: completion.data.choices[0].text});
+  console.log(req.body.text);
+  const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: req.body.text,
+  temperature: 0.7,
+  max_tokens: 500,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+});
+  res.status(200).json({ result: response.data.choices[0].text});
 }
