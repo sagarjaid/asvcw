3e26849bba493242fca8879c89bd239b076a226f
sagar jaid <sagar@sagarjaid.local>
2023-03-20 21:36:32 +0530

    hotjar added 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,29 @@
 import "@/styles/globals.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        onLoad={() => {
+          console.log("hjar");
+        }}
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3415164,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+        }}
+      />
       <Component {...pageProps} />
-      <GoogleAnalytics  />
+      <GoogleAnalytics />
       <Analytics />
     </>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -4,22 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <Script
-        strategy="afterInteractive"
-        onLoad={() => {
-          console.log("hjar");
-        }}
-        dangerouslySetInnerHTML={{
-          __html: `(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3415164,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-        }}
-      />
       <body>
         <Main />
         <NextScript />
