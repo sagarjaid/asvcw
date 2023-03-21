b5d8dc60d40d264b2a1b2048fa9a1d7dd44c1cfc
sagar jaid <sagar@sagarjaid.local>
2023-03-21 09:29:32 +0530

    vistors added

--- pages/_app.js
+++ pages/_app.js
@@ -22,6 +22,22 @@ export default function App({ Component, pageProps }) {
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
         }}
       />
+
+      <Script
+        strategy="afterInteractive"
+        onLoad={() => {
+          console.log("hjar");
+        }}
+        dangerouslySetInnerHTML={{
+          __html: `(function (w,r){w['NextsaleObject']=r;w[r]=w[r]||function(){(w[r].q = w[r].q || []).push(arguments)};})(window, 'nsio')`,
+        }}
+      />
+
+      <Script
+        src="https://sdk.nextsale.io/nextsale.min.js?key=pk_b9846a313b2fe71ca70159baa7ee3e22819c2721"
+        async
+      ></Script>
+
       <Component {...pageProps} />
       <GoogleAnalytics />
       <Analytics />
