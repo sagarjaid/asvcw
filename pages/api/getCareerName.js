6474fa3633ba432badc897633cd0e31c09e68f0b
sagar jaid <sagar@sagarjaid.local>
2023-04-02 13:25:33 +0530

    career route added

---
+++ pages/api/getCareerName.js
@@ -0,0 +1,22 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.NEXT_PUBLIC_CAREER_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req, res) {
+  console.log(req.body.text);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: req.body.text,
+    temperature: 0.7,
+    max_tokens: 1000,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  console.log(response.data.choices[0].text);
+  res.status(200).json({ result: response.data.choices[0].text });
+}
