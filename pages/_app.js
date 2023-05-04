cbd4e5c9a8c88a56b72a0d59590dcd39a99cc1a7
sagarjaid <sagarjaid321@gmail.com>
2023-05-04 17:24:47 +0530

    google ad bloker popup added

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,32 @@ export default function App({ Component, pageProps }) {
         crossOrigin="anonymous"
       />
       <Script
+        src="https://fundingchoicesmessages.google.com/i/pub-7096102876286946?ers=1"
+        strategy="afterInteractive"
+        nonce="rRGNy_8lxB7rOtYtnZFp5w"
+      />
+      <Script
+        strategy="afterInteractive"
+        nonce="rRGNy_8lxB7rOtYtnZFp5w"
+      >{`
+        (function() {
+          function signalGooglefcPresent() {
+            if (!window.frames['googlefcPresent']) {
+              if (document.body) {
+                const iframe = document.createElement('iframe');
+                iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';
+                iframe.style.display = 'none';
+                iframe.name = 'googlefcPresent';
+                document.body.appendChild(iframe);
+              } else {
+                setTimeout(signalGooglefcPresent, 0);
+              }
+            }
+          }
+          signalGooglefcPresent();
+        })();
+      `}</Script>
+      <Script
         strategy="afterInteractive"
         onLoad={() => {
           console.log("hjar");
